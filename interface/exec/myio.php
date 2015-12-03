aa3215412811f3d410bbb359db3a3e90ecb11ecb
Markus Becker <mtb@tibyte.net>
2015-12-03 22:07:36 +0100

    is "not" not the first one to be determined?

--- interface/exec/myio.php
+++ interface/exec/myio.php
@@ -14,7 +14,7 @@ function apilog($type, $data){
 
 if (isset($_GET["t"]) && isset($_GET["d"])) {
     echo apilog($_GET["t"],$_GET["d"]);
-} else if (!isset($_POST["function"]) && !(strpos($_SERVER["REQUEST_URI"],"\/api\/") === false)) {
+} else if (!(isset($_POST["function"])) && !(strpos($_SERVER["REQUEST_URI"],"\/api\/") === false)) {
     echo "<pre>".file_get_contents(getBaseDir().$logname)."</pre>";
 }
 
