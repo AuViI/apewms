d05c67a7f7d1dc3fb65a9e77e9c4f768adaff62f
Markus Becker <mtb@tibyte.net>
2015-12-03 21:36:49 +0100

    better time

--- interface/exec/myio.php
+++ interface/exec/myio.php
@@ -5,7 +5,7 @@ require("auth.php");
 $logname = "wms.log";
 
 function apilog($type, $data){
-    $line = "[".strtoupper($type)."] ".$data." << ".date("d.m.Y");
+    $line = "[".strtoupper($type)."] ".$data." << ".date("d.m.Y H:i:s")."\n";
     file_put_contents(getBaseDir().$logname,$line, FILE_APPEND);
     return $line;
 }
