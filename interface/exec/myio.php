2b9e3cc8ab881d2823f75e2e4673b91ea3239bd1
Markus Becker <mtb@tibyte.net>
2015-12-03 21:33:28 +0100

    working io

--- interface/exec/myio.php
+++ interface/exec/myio.php
@@ -2,14 +2,18 @@
 
 require("auth.php");
 
+logname = "wms.log";
+
 function apilog($type, $data){
-    $line = "[".strtoupper($type)."]".$data." << ".date("d.m.Y");
-    file_put_contents(getBaseDir()."wms.log",$line, FILE_APPEND);
+    $line = "[".strtoupper($type)."] ".$data." << ".date("d.m.Y");
+    file_put_contents(getBaseDir().logname,$line, FILE_APPEND);
     return $line;
 }
 
 if (isset($_GET["t"]) && isset($_GET["d"])) {
     echo apilog($_GET["t"],$_GET["d"]);
+} else {
+    echo file_get_contents(getBaseDir().logname);
 }
 
 ?>
